bc18635a367efece0cc38057924dd71cc618815a
zJunioJz <junioj253@gmail.com>
2024-09-19 16:05:43 -0300

    correcao

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const { Pool } = require('pg');
 const bodyParser = require('body-parser');
