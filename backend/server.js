16bc30358f9a899d62fada32572c2b8691a6f294
zJunioJz <junioj253@gmail.com>
2024-09-28 11:03:27 -0300

    Atualizando server.js

--- backend/server.js
+++ backend/server.js
@@ -27,13 +27,48 @@ pool.connect((err, client, release) => {
   release();
 });
 
-// Rota para acessar um usuário
+// Middleware para autenticar o token
+function authenticateToken(req, res, next) {
+  const token = req.headers['authorization']?.split(' ')[1]; // Extrai o token do cabeçalho
 
+  if (!token) return res.sendStatus(401); 
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403); 
+    req.user = user; // Salva os dados do usuário no objeto de requisição
+    next();
+  });
+}
+
+// Rota para acessar um usuário
+app.get('/user', authenticateToken, async (req, res) => {
+  const userId = req.user.id; 
+  const query = 'SELECT * FROM users WHERE id = $1';
+
+  try {
+    const userResult = await pool.query(query, [userId]);
+
+    if (userResult.rows.length > 0) {
+      res.json(userResult.rows[0]); // Retorna os dados do usuário
+    } else {
+      res.status(404).json({ error: 'Usuário não encontrado' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao buscar dados do usuário' });
+  }
+});
+
+// Rota para verificar se o token é válido
+app.get('/verify-token', authenticateToken, (req, res) => {
+  res.sendStatus(200);
+});
+
+// Rota para login
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Verificar se o usuário existe
     const userQuery = 'SELECT * FROM users WHERE email = $1';
     const userResult = await pool.query(userQuery, [email]);
 
@@ -43,34 +78,30 @@ app.post('/login', async (req, res) => {
 
     const user = userResult.rows[0];
 
-    // Comparar a senha fornecida com a senha armazenada
     const isMatch = await bcrypt.compare(password, user.password);
     
     if (!isMatch) {
       return res.status(401).json({ error: 'E-mail ou senha incorretos.' });
     }
-    // gerar token JWT
+    
     const token = jwt.sign(
-      { id: user.id, email: user.mail },
+      { id: user.id, email: user.email },
       process.env.JWT_SECRET,
-      { expiresIn: '1h' } // tempo no qual o token expira "1 hora"
-    )
+      { expiresIn: '1h' }
+    );
 
-    // Autenticação bem-sucedida
-    res.json({ message: 'Login realizado com sucesso!', user: token });
+    res.json({ message: 'Login realizado com sucesso!', token, user: { id: user.id, email: user.email } });
   } catch (err) {
     console.error(err);
     res.status(500).send(err.message);
   }
 });
 
-
 // Rota para registrar um usuário
 app.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-    // Verificar se o e-mail já existe
     const emailCheckQuery = 'SELECT * FROM users WHERE email = $1';
     const emailCheckResult = await pool.query(emailCheckQuery, [email]);
 
@@ -78,10 +109,8 @@ app.post('/register', async (req, res) => {
       return res.status(400).json({ error: 'Este E-mail já está em uso. Tente outro.' });
     }
 
-    // Criptografar a senha
     const hash = await bcrypt.hash(password, 10);
 
-    // Inserir o novo usuário
     const sql = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)';
     await pool.query(sql, [username, email, hash]);
 
