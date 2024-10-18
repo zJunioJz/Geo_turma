cb3d9628d69b3af07711675c2443463141726306
zJunioJz <junioj253@gmail.com>
2024-10-18 16:44:02 -0300

    ajuste BookingServer

--- backend/BookingServer.js
+++ backend/BookingServer.js
@@ -5,8 +5,6 @@ const cors = require('cors');
 const { Pool } = require('pg');
 
 const app = express();
-
-
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -21,17 +19,17 @@ const pool = new Pool({
 
 // Rota para criar uma nova aula
 app.post('/create-booking', async (req, res) => {
-    const { modalidade, professor, maxAlunos, date, time } = req.body;
+    const { modalidade, professor, maxAlunos, date, start_time, end_time } = req.body;
   
     // Validação básica dos dados recebidos
-    if (!modalidade || !professor || !maxAlunos || !date || !time) {
+    if (!modalidade || !professor || !maxAlunos || !date || !start_time || !end_time) {
       return res.status(400).json({ error: 'Todos os campos devem ser preenchidos.' });
     }
   
     try {
       const result = await pool.query(
-        'INSERT INTO bookings (modalidade, professor, max_alunos, date, time) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-        [modalidade, professor, maxAlunos, date, time]
+        'INSERT INTO bookings (modalidade, professor, max_alunos, date, start_time, end_time) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+        [modalidade, professor, maxAlunos, date, start_time, end_time]
       );
   
       const newBooking = result.rows[0];
